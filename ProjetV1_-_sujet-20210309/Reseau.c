47897ec1485aab25566c8205e8ff924f777bf2af
rayane-47 <makhlouf.rayane2001@gmail.com>
2021-04-26 13:38:08 +0200

    reseau

--- ProjetV1_-_sujet-20210309/Reseau.c
+++ ProjetV1_-_sujet-20210309/Reseau.c
@@ -2,8 +2,14 @@
 Noeud* rechercheCreeNoeudListe(Reseau *R, double x, double y){
     CellNoeud *tmp=R->noeuds;
     Noeud *res=NULL;
-    while(tmp){
+    while(tmp!=NULL){
+        printf("r_x: %f\n",tmp->nd->x);
+        printf("r_y: %f\n",tmp->nd->y);
+        printf("x=%f : y=%f\n",x,y);
+        printf("------------------\n");
+        //printf("x_suiv :%f , y_suiv :%f\n",tmp->suiv->nd->x,tmp->suiv->nd->y);
         if(tmp->nd->x==x && tmp->nd->y==y){ // Si on trouve le noeud correspondant
+            printf("Passe\n");
             res=tmp->nd;
             return res;
         }
@@ -21,6 +27,7 @@ Noeud* rechercheCreeNoeudListe(Reseau *R, double x, double y){
     res->voisins=NULL;
     CellNoeud *newCell=(CellNoeud *)malloc(sizeof(CellNoeud));
     if(newCell==NULL){
+        printf("Erreur d'allocation memoire\n");
         return NULL;
     }
     newCell->nd=res;
@@ -29,7 +36,7 @@ Noeud* rechercheCreeNoeudListe(Reseau *R, double x, double y){
     R->nbNoeuds++;
     return res;
 }
-Reseau* reconstitueReseauListe(Chaines *C){
+/*Reseau* reconstitueReseauListe(Chaines *C){
     CellChaine *tmp1=C->chaines;
     Reseau *res=(Reseau*)malloc(sizeof(Reseau));
     if(res==NULL){
@@ -68,9 +75,77 @@ Reseau* reconstitueReseauListe(Chaines *C){
         tmp3->suiv=tmp4;
         tmp3=tmp4;
     }
+}*/
+void ajouteVoisinDebut(Noeud *n, CellNoeud *v){
+    if(n->voisins==NULL){
+        n->voisins=v;
+    }else{
+        v->suiv=n->voisins;
+        n->voisins=v;
+    }
 }
+Reseau* reconstitueReseauListe(Chaines *C){
+    Reseau * res=(Reseau *)malloc(sizeof(Reseau));
+    if(res==NULL){
+        printf("Erreur d'allocation memoire\n");
+        return NULL;
+    }
+    res->gamma=C->gamma; // Nombre maximum de fibres par cable
+    res->nbNoeuds=0; // Nombre de noeuds du reseau est initialement null
+    res->noeuds=NULL;
+    res->commodites=NULL;
+    CellChaine * tmp1=C->chaines; // Liste chainee des chaines
+    int cpt=0;
+    CellCommodite *c=res->commodites;
+    Noeud *n=NULL;
+    while(tmp1){
+        printf("debut\n");
+        CellPoint *tmp2=tmp1->points; // Liste des points de chaque chaine
+        CellPoint *tmp3=NULL;
+        CellNoeud *prec=NULL;
+        //printf("passe1\n");
+        n=rechercheCreeNoeudListe(res,tmp2->x,tmp2->y);
+        //printf("passe2\n");
+        prec=res->noeuds;
+        c=(CellCommodite *)malloc(sizeof(CellCommodite));
+            if(c==NULL){
+                printf("Erreur d'allocation\n");
+                return NULL;
+            }
+        c->extrA=n;
+        tmp2=tmp2->suiv;
+        while(tmp2){
+            //printf("it: %d\n",cpt);
+            printf("passe1\n");
+            n=rechercheCreeNoeudListe(res,tmp2->x,tmp2->y);
+            printf("passe2\n");
+            ajouteVoisinDebut(n,prec);
+            ajouteVoisinDebut(prec->nd,res->noeuds);
+            prec=res->noeuds;
+            tmp3=tmp2;
+            tmp2=tmp2->suiv;
+            //printf("fin it: %d\n",cpt);
+        }
+        c->extrB=rechercheCreeNoeudListe(res,tmp3->x,tmp3->y);
+        cpt++;
+        tmp1=tmp1->suiv;
+        c=c->suiv;
+        printf("fin\n");
+    }
+    return res;
+}
+
 void liberer_noued(Noeud *nd){
-    free(nd);
+    if(nd){
+        CellNoeud *tmp=nd->voisins;
+        CellNoeud *tmp2=NULL;
+        while(tmp){
+            tmp2=tmp->suiv;
+            free(tmp);
+            tmp=tmp2;
+        }
+        free(nd);
+    }
 }
 void liberer_liste_noeuds(CellNoeud *lNd){
     CellNoeud *tmp=NULL;
@@ -94,3 +169,111 @@ void liberer_reseau(Reseau *r){
     liberer_liste_commodites(r->commodites);
     free(r);
 }
+
+/*int nbCommodites(Reseau *R){
+    int cpt=0;
+    CellCommodite *c=R->commodites;
+    while(c){
+        cpt++;
+        c=c->suiv;
+    }
+    return cpt;
+}
+int nbLiaisons(Reseau *R){
+    CellNoeud *listeN1=R->noeuds;
+    int *tab=malloc(sizeof(int)*500);
+    int index=0;
+    int cpt=0;
+    int bool=0;
+    while(listeN1){
+        Noeud *n=listeN1->nd;
+        CellNoeud *v=n->voisins;
+        while(v){
+            for(int i=0;i<500;i++){
+                if(tab[i]==v->nd->num)
+                    bool=1;
+
+            }
+            if(bool==0 ){
+                if(index<500){
+                    cpt++;
+                    tab[index]=v->nd->num;
+                    index++;
+                }else{
+                    printf("Tableau plein!!");
+                }
+            }
+            bool=0;
+            v=v->suiv;
+
+        }
+        listeN1=listeN1->suiv;
+    }
+    free(tab);
+    return cpt;
+}
+void ecrireReseau(Reseau *R, FILE *f){
+    if(f!=NULL){
+        fprintf(f,"NbNoeuds: %d\n",R->nbNoeuds);
+        fprintf(f,"NbLiaisons: %d\n",nbLiaisons(R));
+        fprintf(f,"NbCommodites: %d\n\n\n",nbCommodites(R));
+        CellNoeud *noeuds=R->noeuds;
+        while(noeuds){
+            fprintf(f,"v %d %f %f\n",noeuds->nd->num,noeuds->nd->x,noeuds->nd->y);
+            noeuds=noeuds->suiv;
+        }
+        int *tab=malloc(sizeof(int)*500);
+        int bool=0;
+        noeuds=R->noeuds;
+        while(noeuds){
+            for(int i=0;i<500;i++){
+                if(tab[i]==noeuds->nd->num)
+                    bool=1;
+            }
+            if(bool==0){
+                CellNoeud *v=noeuds->nd->voisins;
+                while(v){
+                    fprintf(f,"l %d %d\n",noeuds->nd->num,v->nd->num);
+                    v=v->suiv;
+                }
+            }
+            bool=0;
+            noeuds=noeuds->suiv;
+        }
+        CellCommodite *commodites=R->commodites;
+        while(commodites){
+            fprintf(f,"k %d %d",commodites->extrA->num,commodites->extrB->num);
+            commodites=commodites->suiv;
+        }
+        fclose(f);
+    }else{
+        printf("Fichier passe en parametre non ouvert");
+    }
+}
+void afficheReseauSVG(Reseau *R, char* nomInstance){
+    CellNoeud *courN,*courv;
+    SVGwriter svg;
+    double maxx=0,maxy=0,minx=1e6,miny=1e6;
+
+    courN=R->noeuds;
+    while (courN!=NULL){
+        if (maxx<courN->nd->x) maxx=courN->nd->x;
+        if (maxy<courN->nd->y) maxy=courN->nd->y;
+        if (minx>courN->nd->x) minx=courN->nd->x;
+        if (miny>courN->nd->y) miny=courN->nd->y;
+        courN=courN->suiv;
+    }
+    SVGinit(&svg,nomInstance,500,500);
+    courN=R->noeuds;
+    while (courN!=NULL){
+        SVGpoint(&svg,500*(courN->nd->x-minx)/(maxx-minx),500*(courN->nd->y-miny)/(maxy-miny));
+        courv=courN->nd->voisins;
+        while (courv!=NULL){
+            if (courv->nd->num<courN->nd->num)
+                SVGline(&svg,500*(courv->nd->x-minx)/(maxx-minx),500*(courv->nd->y-miny)/(maxy-miny),500*(courN->nd->x-minx)/(maxx-minx),500*(courN->nd->y-miny)/(maxy-miny));
+            courv=courv->suiv;
+        }
+        courN=courN->suiv;
+    }
+    SVGfinalize(&svg);
+}*/
